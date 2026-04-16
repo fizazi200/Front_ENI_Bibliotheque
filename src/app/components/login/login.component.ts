f54bc538839452543c5edcf5d555e8a3f8a67f07
fizazi200 <fizazi2021@gmail.com>
2026-04-16 12:54:35 +0200

    creation de page loginet inscription

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
+  isLoginMode = signal(true);
+
+  authForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.authForm = this.fb.group({
+
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
+      ]],
+
+      nom: [''],
+      prenom: ['']
+    });
+
+    this.updateValidators();
+  }
+
+  toggleMode() {
+    this.isLoginMode.update(v => !v);
+    this.updateValidators();
+  }
+
+  private updateValidators() {
+
+    const isLogin = this.isLoginMode();
+
+    const nom = this.authForm.get('nom');
+    const prenom = this.authForm.get('prenom');
+
+    if (!isLogin) {
+
+      nom?.setValidators([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern(/^[A-Za-zÀ-ÿ\s-]+$/)
+      ]);
+
+      prenom?.setValidators([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern(/^[A-Za-zÀ-ÿ\s-]+$/)
+      ]);
+
+      nom?.enable();
+      prenom?.enable();
+
+    } else {
+
+      nom?.clearValidators();
+      prenom?.clearValidators();
+
+      nom?.setValue('');
+      prenom?.setValue('');
+
+      nom?.disable();
+      prenom?.disable();
+    }
+
+    nom?.updateValueAndValidity();
+    prenom?.updateValueAndValidity();
+  }
+
+  onSubmit() {
+
+    if (this.authForm.invalid) return;
+
+    if (this.isLoginMode()) {
+
+      const loginData = {
+        email: this.authForm.value.email,
+        password: this.authForm.value.password
+      };
+
+      console.log("LOGIN :", loginData);
+
+    } else {
+
+      const registerData = {
+        nom: this.authForm.value.nom,
+        prenom: this.authForm.value.prenom,
+        email: this.authForm.value.email,
+        password: this.authForm.value.password
+      };
+
+      console.log("REGISTER :", registerData);
+    }
+
+    // 👉 appel backend Spring Boot ici
+  }
 }

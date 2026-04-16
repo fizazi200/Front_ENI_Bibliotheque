797cf127944b4cfe5c2800e2ab113fbff2ec9264
fizazi200 <fizazi2021@gmail.com>
2026-04-16 17:09:19 +0200

    feat: ajout de traitement de connexion

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -87,26 +87,24 @@ export class LoginComponent {
 
     if (this.authForm.invalid) return;
 
-    if (this.isLoginMode()) {
 
       const loginData = {
         email: this.authForm.value.email,
         password: this.authForm.value.password
       };
 
+      this.authService.login(loginData).subscribe({
+        next: () => {
+          console.log("Login success");
+          alert("Login success");
+        },
+        error: (err) => {
+          console.error("Erreur login", err);
+        }
+      });
+
       console.log("LOGIN :", loginData);
 
-    } else {
-
-      const registerData = {
-        nom: this.authForm.value.nom,
-        prenom: this.authForm.value.prenom,
-        email: this.authForm.value.email,
-        password: this.authForm.value.password
-      };
-
-      console.log("REGISTER :", registerData);
-    }
 
     // 👉 appel backend Spring Boot ici
   }
